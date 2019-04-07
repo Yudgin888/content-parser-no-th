740b7b8e1dcfc1f366592da354177f0dbd726b3d
yudgin <yudgin88888@gmail.com>
2019-04-07 22:25:16 +0300

    pre-release

--- Urlparser.php
+++ Urlparser.php
@@ -1,48 +1,47 @@
 <?php
 require_once "simplehtmldom/simple_html_dom.php";
+require_once "mysql_wrapper.php";
 set_time_limit(0);
-define("INTERNAL", 1); // только внутренние
-define("EXTERNAL", 2); // только внешние
-define("ALL", 3); // внутренние и внешние (переход на внешние не делается)
+define("INTERNAL", 1);
+define("EXTERNAL", 2);
 
 class Urlparser
 {
-    public $max_depth; // глубина прохода по ссылкам
-    public $checkstatus; // проверка http статуса
-    public $transition; // стратегия отсеивания ссылок (INTERNAL / EXTERNAL / ALL)
-
     private $save_to_db;
-    private $starturl;
     private $root; // корневой адрес сайта
+    private $start_url;
+    private $all_directions; // проход по ссылкам (false - не ниже стартовой)
+    private $del_get;
 
     public $links; // обработанные ссылки
-    // структура
+    //      структура
     // 'url' => $url
     // 'status' => http статус
-    // 'depth' => глубина ссылки от начальной
-    // 'parrent' => родительская ссылка
-    // 'type' => тип ссылки
 
-    private $unique_links; // массив уникальных ссылок
+    public $unique_links; // массив уникальных ссылок
 
-    public $internal_counter;
-    public $external_counter;
+    public $products;
+    // 'name' - название
+    // 'url' - страница товара
+    // 'article' - артикул
+    // 'price' - цена
+    // 'description' - описание
+    // 'images' - json
+    // 'dir_img' - папка с изобр
 
     public function __construct()
     {
         $this->unique_links = [];
         $this->links = [];
-        $this->internal_counter = 0;
-        $this->external_counter = 0;
+        $this->products = [];
     }
 
-    public function parse($url, $checkstatus = false, $transition = INTERNAL, $max_depth = 0, $save_to_db = false)
+    public function parse($url, $all_directions = false, $del_get = true, $save_to_db = false)
     {
-        $this->max_depth = $max_depth;
+        $this->del_get = $del_get;
         $this->save_to_db = $save_to_db;
-        $this->checkstatus = $checkstatus;
-        $this->transition = $transition;
-        $this->starturl = $url;
+        $this->all_directions = $all_directions;
+        $this->start_url = $url;
         $parts = parse_url($url);
         $this->root = $parts['scheme'] . '://' . $parts['host'];
         $this->unique_links[$url] = 1;
@@ -53,51 +52,64 @@ class Urlparser
         }
     }
 
-    private function worker($url, $depth = 0, $parrent_url = null)
+    private function worker($url)
     {
-        if ($this->max_depth != 0 && $depth > $this->max_depth) {
-            return;
-        }
         $typeUrl = $this->checkTypeUrl($url);
-        if ($typeUrl === $this->transition || $this->transition === ALL) {
+        if ($typeUrl === INTERNAL) {
             $arr = [
                 'url' => $url,
-                'status' => $this->checkstatus ? $this->getStatus($url) : null,
-                'depth' => $depth,
-                'parrent' => $parrent_url,
-                'type' => $typeUrl,
+                'status' => $this->getStatus($url),
             ];
             $this->links[] = $arr;
-            if ($typeUrl === INTERNAL) {
-                $this->internal_counter++;
-            } else {
-                $this->external_counter++;
-            }
-            echo $arr['url'] . ' - status: ' . $arr['status'] . ' - depth: ' . $arr['depth'] . '<br>';
+            echo $arr['url'] . ' - status: ' . $arr['status'] . '<br>';
             flush();
-        }
-        if ($typeUrl === INTERNAL) {
-            $this->page_parser($url, $depth + 1);
+            $this->page_parser($url);
         }
     }
 
-    private function page_parser($url, $depth)
+    private function page_parser($url)
     {
         $html = new simple_html_dom();
         try {
             $html->load_file($url);
             if ($html !== null && is_object($html) && isset($html->nodes) && count($html->nodes) > 0) {
+                $product_card = $html->find('.catalog_detail .item_main_info');
+                foreach ($product_card as $item) {
+                    $name = $html->find('#pagetitle')[0]->innertext;
+                    $article = $item->find('.article .value')[0]->innertext;
+                    $price = $item->find('.prices_block .price_value')[0]->innertext . $item->find('.prices_block .price_currency')[0]->innertext;
+                    $description = $item->find('.preview_text')[0]->innertext;
+                    $imgs = $item->find('div.slides li a[href]');
+                    $img_src = [];
+                    foreach ($imgs as $img) {
+                        $img_src[] = $img->attr['href'];
+                    }
+                    $dir = $this->downloadImages($img_src);
+
+                    $product = [
+                        'name' => $name,
+                        'url' => $url,
+                        'article' => $article,
+                        'price' => $price,
+                        'description' => $description,
+                        'images' => json_encode($img_src),
+                        'dir_img' => $dir
+                    ];
+                    $this->products[] = $product;
+                }
+
                 $alllinks = $html->find('a[href]');
                 foreach ($alllinks as $link) {
                     $href = $link->attr['href'];
-                    if ($href != null) {
-                        if (preg_match('/\.(png|jpeg|gif|jpg|js|css|xml|pdf)/', $href)) {
+                    if ($href != null && preg_match('/(\/catalog\/product\/).+/', $href)) {
+                        if (preg_match('/\.(png|jpeg|gif|jpg|js|css|xml|pdf)/', $href)
+                            || preg_match('/.((%3Bamp%3Bamp)|(sort=)|(set_filter=))/', $href)) {
                             continue;
                         }
                         $href = $this->prepareUrl($href);
-                        if (!isset($this->unique_links[$href])) {
+                        if (!isset($this->unique_links[$href]) && $this->checkUrlLevel($href)) {
                             $this->unique_links[$href] = 1;
-                            $this->worker($href, $depth, $url);
+                            $this->worker($href);
                         }
                     }
                 }
@@ -106,6 +118,27 @@ class Urlparser
             echo 'Error: ' . $url . PHP_EOL;
         }
         $html->clear();
+    }
+
+    public function downloadImages($arr)
+    {
+        $dir = null;
+        if (count($arr) > 0) {
+            $dir = $_SERVER['DOCUMENT_ROOT'] . '/upload/' . time();
+            mkdir($dir, 0777, true);
+            foreach ($arr as $item){
+                $url = $this->root . $item;
+                $ch = curl_init($url);
+                $filename = $dir . '/' . basename($item);
+                $fp = fopen($filename, 'wb');
+                curl_setopt($ch, CURLOPT_FILE, $fp);
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+                curl_exec($ch);
+                curl_close($ch);
+                fclose($fp);
+            }
+        }
+        return $dir;
     }
 
     public function getStatus($url)
@@ -118,13 +151,7 @@ class Urlparser
 
     public function saveToDB($overwrite)
     {
-        if (file_exists("mysql_wrapper.php")) {
-            include_once "mysql_wrapper.php";
-            saveAll($this->links, $overwrite);
-            return true;
-        } else {
-            return false;
-        }
+        saveAll($this->links, $overwrite);
     }
 
     public function checkTypeUrl($url)
@@ -150,9 +177,19 @@ class Urlparser
         } elseif (!isset($parts['scheme'])) {
             $url = 'http:' . $url;
         }
-        if(isset($parts['fragment'])){
+        if (isset($parts['fragment'])) {
             $url = substr($url, 0, strpos($url, '#'));
         }
+        if ($this->del_get && isset($parts['query'])) {
+            $url = substr($url, 0, strpos($url, '?'));
+        }
         return $url;
+    }
+
+    private function checkUrlLevel($url)
+    {
+        if ($this->all_directions || (!$this->all_directions && strstr($url, $this->start_url))) {
+            return true;
+        } else return false;
     }
 }
--- index.php
+++ index.php
@@ -4,17 +4,12 @@ ini_set("display_errors",1);
 error_reporting(E_ALL);
 $start = microtime(true);
 
-$url = "http://simonenko.su";
-//$url = "https://wiki.pwodev.com";
-
+$url = "http://zanachka.isumkaby.vh94.hosterby.com/catalog/category/chemodany_na_2_kolesakh";
 $parser = new Urlparser();
-$parser->parse($url, true, ALL, 0, true);
+$parser->parse($url, true, true, false);
+
 
 echo '<br>Done!<br>';
-echo 'Всего: ' . count($parser->links) . '<br>';
-echo 'Уникальных: ' . ($parser->internal_counter + $parser->external_counter) . '<br>';
-echo 'Всего внутренних: ' . $parser->internal_counter . '<br>';
-echo 'Всего внешних: ' . $parser->external_counter . '<br>';
-$time = microtime(true) - $start;
-echo 'Time: ' . $time . '<br>';
+echo 'Total url: ' . count($parser->links) . '<br>';
+echo 'Time: ' . (microtime(true) - $start) . '<br>';
 die;
